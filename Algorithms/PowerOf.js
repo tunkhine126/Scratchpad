ed806f10ac62ff5f667bd6012f479117385337df
tunkhine126 <tunkhine126@gmail.com>
2019-11-01 10:26:43 -0400

    PowerOf

---
+++ Algorithms/PowerOf.js
@@ -0,0 +1,19 @@
+function power(x, n) {
+  let ans = x
+
+  if (n < 0) {
+    // accounting for edge cases of the answer
+    ans = 1.0 / ans
+    // accounting for edge cases of the variable
+    x = 1.0 / x
+  }
+
+  // loop with absolute value of n
+  for (let i = 0; i < (Math.abs(n) - 1); i++) {
+    ans *= x
+  }
+  return ans
+}
+
+console.log(power(2, 6))
+console.log(power(-1, 5))
