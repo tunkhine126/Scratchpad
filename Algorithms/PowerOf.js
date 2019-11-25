1317864ce7a7f99a29599ead25efc070c9698a09
tunkhine126 <tunkhine126@gmail.com>
2019-11-25 12:49:40 -0500

    Refactored PowerOf

--- Algorithms/PowerOf.js
+++ Algorithms/PowerOf.js
@@ -17,3 +17,18 @@ function power(x, n) {
 
 console.log(power(2, 6))
 console.log(power(-1, 5))
+
+// refactored 
+
+function power(x, n) {
+  let ans = x
+  // loop with absolute value of n
+  for (let i = 0; i < (Math.abs(n) - 1); i++) {
+    ans *= x
+  }
+  if (n < 0) {
+    // accounting for edge cases of the answer
+    ans = 1.0 / ans
+  }
+  return ans
+}
