ea6bd764d3d6ea511ed121a0405319b6a0f73d6f
Tun Khine <tunkhine126@gmail.com>
2023-01-30 19:53:38 -0500

    Control structures

---
+++ Algorithms/HackerRank/ruby/ControlStructures/infininte_loop.rb
@@ -0,0 +1,33 @@
+# This is a wonderful exercise which explains the concept of infinite loops.
+
+# "A hacker practices on HackerRank until getting to a rating of O(1) read as (Oh-one)"
+
+# An infinite loop in Ruby is of the form
+
+# loop do
+# end
+# Use an infinite loop and call the method coder.practice within it and break if coder.oh_one? is true.
+
+# break if conditions in Ruby are of the form
+
+# if <condition>
+#     break
+# end
+# or a one-liner
+
+# break if <condition>
+
+loop do
+  break if coder.oh_one?
+   coder.practice
+end
+
+# or
+
+loop do
+  if coder.oh_one?
+      break
+  else
+       coder.practice
+  end
+end
